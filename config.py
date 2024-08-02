1546229b42de893b4e4d9e31c626c0bb62edbfbc
Sagar Jaypuria <113926118+sagar-jay001@users.noreply.github.com>
2024-08-02 19:03:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7232045428:AAFwxbhrEaUtINsvRpD32Ak5B8j3pZKNPPs")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1994270625")
 
 
 
